c48553b7c8c0607452637ff3e0df9091533bd914
Victor André Gris Costa <victoragcosta@hotmail.com>
2017-03-12 01:54:38 -0300

    Touch support added

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var snake;
 var food;
 var game;
 
+var touchPoints = [];
+
 function setup() {
 	createCanvas(600, 600);	
 	
@@ -40,5 +42,51 @@ function keyPressed(){
 		snake.dir(-1, 0);
 	} else if(keyCode === RIGHT_ARROW){
 		snake.dir(1,0);
+	}
+}
+
+function touchStarted(){
+	touchPoints[0] = createVector(mouseX, mouseY);
+}
+
+function touchEnded(){
+	touchPoints[1] = createVector(mouseX, mouseY);
+	handleTouches();
+}
+
+function touchMoved(){
+	return false;
+}
+
+function handleTouches(){
+	var touchDir = touchPoints[1].copy().sub(touchPoints[0]).normalize();
+	if(touchDir.x === abs(touchDir.x)){
+		if(touchDir.y === abs(touchDir.y)){
+			if(touchDir.x > touchDir.y){
+				snake.dir(1,0);
+			} else {
+				snake.dir(0,1);
+			}
+		} else {
+			if(touchDir.x > abs(touchDir.y)){
+				snake.dir(1,0);
+			} else {
+				snake.dir(0,-1);
+			}
+		}
+	} else {
+		if(touchDir.y === abs(touchDir.y)){
+			if(abs(touchDir.x) > touchDir.y){
+				snake.dir(-1,0);
+			} else {
+				snake.dir(0,1);
+			}
+		} else {
+			if(touchDir.x < touchDir.y){
+				snake.dir(-1,0);
+			} else {
+				snake.dir(0,-1);
+			}
+		}
 	}
 }
